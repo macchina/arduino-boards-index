432655464162e21cf251a0bdfc5c76f5bf8b0bbd
Drew Fustini <pdp7pdp7+pdp7github@gmail.com>
2019-05-06 12:31:25 +0200

    add logic to remove releases without platform.json

--- index.js
+++ index.js
@@ -24,16 +24,34 @@ async function getPlatform(release) {
   const platform = release.assets.filter(a => a.name === 'platform.json')[0]
   // skip this release if platform.json does not exist
   if(platform == null) {
-    return;
+    return
   }
   return JSON.parse(await rp(platform.browserDownloadUrl));
+}
+
+function clean(obj) {
+  for (var propName in obj) { 
+    if (obj[propName] === null || obj[propName] === undefined) {
+      delete obj[propName];
+    }
+  }
 }
 
 async function main() {
 
   // NB. this is Flat-map
   releaseArrays = await Promise.all(publishingRepositories.map(getReleases))
-  const releases = [].concat.apply([], releaseArrays);
+  const releases = [].concat.apply([], releaseArrays)
+  plat = await Promise.all(releases.map(getPlatform))
+  // remove any releases for which there is no associated platform.json
+  clean(plat);
+  var platformArray = []
+  for (var i in plat) {
+      platformArray.push(plat[i])
+  }
+  //for (var i in platformArray) {
+  //    console.error("platformArray[" + i + "]=" + JSON.stringify(platformArray[i], null, 2));
+  //}
 
   const index = {
     packages: [
@@ -45,7 +63,7 @@ async function main() {
         help: {
           online: "https://forum.macchina.cc/"
         },
-        platforms: await Promise.all(releases.map(getPlatform)),
+        platforms: platformArray,
         tools: []
       }
     ]
